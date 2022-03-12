08b5c009a67085a29897be98e507012090babb21
Adrian <adriantutuianu01@gmail.com>
2022-03-12 22:44:15 +0000

    Cleaned cra

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import About from "./Sections/About";
 import Home from "./Sections/Home";
@@ -8,20 +7,6 @@ import Contact from "./Sections/Contact";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <About />
       <Home />
       <Projects />
