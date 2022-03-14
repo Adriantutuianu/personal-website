a1824f45ca7884fad0bab8ee57a68c67a1d7c3fb
Adrian <adriantutuianu01@gmail.com>
2022-03-14 22:38:46 +0000

    Aligned header menu

--- src/Header.js
+++ src/Header.js
@@ -5,46 +5,51 @@ import "./header.scss";
 const Header = () => {
   return (
     <header className="header">
-      <Link
-        activeClass="active"
-        to="home-section"
-        spy={true}
-        smooth={true}
-        duration={900}
-        delay={100}
-      >
-        Home
-      </Link>{" "}
-      <Link
-        activeClass="active"
-        to="about-section"
-        spy={true}
-        smooth={true}
-        duration={900}
-        delay={100}
-      >
-        About
-      </Link>{" "}
-      <Link
-        activeClass="active"
-        to="projects-section"
-        spy={true}
-        smooth={true}
-        duration={900}
-        delay={100}
-      >
-        Projects
-      </Link>{" "}
-      <Link
-        activeClass="active"
-        to="contact-section"
-        spy={true}
-        smooth={true}
-        duration={900}
-        delay={100}
-      >
-        Contact
-      </Link>{" "}
+      <div className="content-wrap">
+        <p className="logo">LOGO</p>
+        <nav className="navigation-menu ">
+          <Link
+            activeClass="active"
+            to="home-section"
+            spy={true}
+            smooth={true}
+            duration={900}
+            delay={100}
+          >
+            Home
+          </Link>{" "}
+          <Link
+            activeClass="active"
+            to="about-section"
+            spy={true}
+            smooth={true}
+            duration={900}
+            delay={100}
+          >
+            About
+          </Link>{" "}
+          <Link
+            activeClass="active"
+            to="projects-section"
+            spy={true}
+            smooth={true}
+            duration={900}
+            delay={100}
+          >
+            Projects
+          </Link>{" "}
+          <Link
+            activeClass="active"
+            to="contact-section"
+            spy={true}
+            smooth={true}
+            duration={900}
+            delay={100}
+          >
+            Contact
+          </Link>{" "}
+        </nav>
+      </div>
     </header>
   );
 };
