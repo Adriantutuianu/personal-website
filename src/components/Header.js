883721a25e609ee56bc7f3cb2284788fb7910784
Adrian <adriantutuianu01@gmail.com>
2022-03-16 23:27:48 +0000

    Moved menu in it`s own component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,55 +1,13 @@
 import React from "react";
-import { Link } from "react-scroll";
 import "./header.scss";
+import NavigationMenu from "./NavigationMenu";
 
 const Header = ({ setTitle }) => {
-  const menu = [
-    {
-      id: 1,
-      path: "home-section",
-      title: "Home - Adrian Tut",
-      name: "Home",
-    },
-    {
-      id: 2,
-      path: "about-section",
-      title: "About - Adrian Tut",
-      name: "About",
-    },
-    {
-      id: 3,
-      path: "projects-section",
-      title: "Projects - Adrian Tut",
-      name: "Projects",
-    },
-    {
-      id: 4,
-      path: "contact-section",
-      title: "Contact - Adrian Tut",
-      name: "Contact",
-    },
-  ];
-
-  const navigationMenu = menu.map((item) => (
-    <Link
-      key={item.id}
-      activeClass="active"
-      to={item.path}
-      spy={true}
-      smooth={true}
-      duration={900}
-      delay={100}
-      onClick={() => setTitle(item.title)}
-    >
-      {item.name}
-    </Link>
-  ));
-
   return (
     <header className="header">
       <div className="content-wrap">
         <p className="logo">LOGO</p>
-        <nav className="navigation-menu ">{navigationMenu}</nav>
+        <NavigationMenu setTitle={setTitle} />
       </div>
     </header>
   );
---
+++ src/components/NavigationMenu.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link } from "react-scroll";
+import "./navigationMenu.scss";
+
+const NavigationMenu = ({ setTitle }) => {
+  const menu = [
+    {
+      id: 1,
+      path: "home-section",
+      title: "Home - Adrian Tut",
+      name: "Home",
+    },
+    {
+      id: 2,
+      path: "about-section",
+      title: "About - Adrian Tut",
+      name: "About",
+    },
+    {
+      id: 3,
+      path: "projects-section",
+      title: "Projects - Adrian Tut",
+      name: "Projects",
+    },
+    {
+      id: 4,
+      path: "contact-section",
+      title: "Contact - Adrian Tut",
+      name: "Contact",
+    },
+  ];
+
+  const navigationMenu = menu.map((item) => (
+    <Link
+      key={item.id}
+      activeClass="active"
+      to={item.path}
+      spy={true}
+      smooth={true}
+      duration={900}
+      delay={100}
+      onClick={() => setTitle(item.title)}
+    >
+      {item.name}
+    </Link>
+  ));
+
+  return <nav className="navigation-menu ">{navigationMenu}</nav>;
+};
+
+export default NavigationMenu;
