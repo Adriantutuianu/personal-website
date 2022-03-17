41c337db98e52443d16e7c6a59fdad8e693136c5
Adrian <adriantutuianu01@gmail.com>
2022-03-17 23:28:23 +0000

    Added contact content- mail

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -1,13 +1,24 @@
 import React from "react";
 import "./contact.scss";
 import { Element } from "react-scroll";
+import { ReactComponent as MailImage } from "../icons/mail-image.svg";
 
 const Contact = () => {
   return (
     <Element name="contact-section" id="contact" className="section">
       <h1>
-        Let`s get <span> in touch </span>{" "}
+        Let`s get <span> in touch </span>
       </h1>
+      <div className="contact-wrap">
+        <a
+          className="item item--mail"
+          href="mailto:adrian.tut20@yahoo.com?subject=Contact Adrian"
+          title="Email Adrian at adrian.tut20@yahoo.com"
+        >
+          <MailImage />
+          <span>adrian.tut20@yahoo.com</span>
+        </a>
+      </div>
     </Element>
   );
 };
