b5974b2b42a229ac5cb9048a49b739259a1cbe36
Adrian <adriantutuianu01@gmail.com>
2022-03-17 23:40:47 +0000

    Added contact content-phone

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./contact.scss";
 import { Element } from "react-scroll";
 import { ReactComponent as MailImage } from "../icons/mail-image.svg";
+import { ReactComponent as PhoneImage } from "../icons/phone-logo.svg";
+import { ReactComponent as LinkedInImage } from "../icons/linkedin-logo.svg";
 
 const Contact = () => {
   return (
@@ -10,6 +12,10 @@ const Contact = () => {
         Let`s get <span> in touch </span>
       </h1>
       <div className="contact-wrap">
+        <div className="item item--phone">
+          <PhoneImage />
+          <span>07927753503</span>
+        </div>
         <a
           className="item item--mail"
           href="mailto:adrian.tut20@yahoo.com?subject=Contact Adrian"
