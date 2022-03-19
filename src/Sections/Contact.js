9c29ca82fa86079239553602f6396d143d6505f8
Adrian <adriantutuianu01@gmail.com>
2022-03-19 23:09:53 +0000

    Deleted phone from contact

--- src/Sections/Contact.js
+++ src/Sections/Contact.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./contact.scss";
 import { Element } from "react-scroll";
 import { ReactComponent as MailImage } from "../icons/mail-image.svg";
-import { ReactComponent as PhoneImage } from "../icons/phone-logo.svg";
 import { ReactComponent as LinkedInImage } from "../icons/linkedin-logo.svg";
 
 const Contact = () => {
@@ -12,10 +11,6 @@ const Contact = () => {
         Let`s get <span> in touch </span>
       </h1>
       <div className="contact-wrap">
-        <div className="item item--phone">
-          <PhoneImage />
-          <span>07927753503</span>
-        </div>
         <a
           className="item item--mail"
           href="mailto:adrian.tut20@yahoo.com?subject=Contact Adrian"
