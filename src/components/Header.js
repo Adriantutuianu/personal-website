740a0cdc956234a0d1c9d704c29ca8e774d0986e
Adrian <adriantutuianu01@gmail.com>
2022-03-20 22:45:27 +0000

    Added logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./header.scss";
 import NavigationMenu from "./NavigationMenu";
+import logo from "../icons/adrian-tut-logo.png";
 
 const Header = ({ setTitle }) => {
   return (
     <header className="header">
       <div className="content-wrap">
-        <p className="logo">LOGO</p>
+        <img
+          className="logo"
+          style={{ height: "100%" }}
+          src={logo}
+          alt="Product logo"
+        />
         <NavigationMenu setTitle={setTitle} />
       </div>
     </header>
