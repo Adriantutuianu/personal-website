b155dd8f58e8774c7d3fc9989eca71620115071a
Adrian <adriantutuianu01@gmail.com>
2022-03-23 22:42:16 +0000

    Styled text-content in about section

--- src/Sections/About.js
+++ src/Sections/About.js
@@ -6,28 +6,32 @@ import ScrollTo from "../components/ScrollTo";
 const About = ({ setTitle }) => {
   return (
     <Element name="about-section" id="about" className="section">
-      <h1>Me, Myself & I </h1>
-      <p>
-        I am 33 years old and last year I made a decision that I am proud of -
-        decided and committed to absorb as much information as I can about
-        building projects.
-      </p>
-      <p>
-        I have started to learn HTML5 , CSS , SCSS , JAVASCRIPT and ending with
-        REACT .
-      </p>
-      <p>
-        You can see the progress of my journey on Github and on LinkedIn all
-        certificates that I gained.
-      </p>
-      <p>
-        In my spare time I like DIY, swimming, outdoor activities , travel and
-        building my own projects. A family person and father of 3 years old son.
-      </p>
-      <p>
-        Interested in the entire frontend spectrum and working on ambitious
-        projects with positive people.
-      </p>
+      <div className="text-content">
+        {" "}
+        <span>Me, Myself & I </span>
+        <p>
+          I am 33 years old and last year I made a decision that I am proud of -
+          decided and committed to absorb as much information as I can about
+          building projects.
+        </p>
+        <p>
+          I have started to learn HTML5 , CSS , SCSS , JAVASCRIPT and ending
+          with REACT .
+        </p>
+        <p>
+          You can see the progress of my journey on Github and on LinkedIn all
+          certificates that I gained.
+        </p>
+        <p>
+          In my spare time I like DIY, swimming, outdoor activities , travel and
+          building my own projects. A family person and father of 3 years old
+          son.
+        </p>
+        <p>
+          Interested in the entire frontend spectrum and working on ambitious
+          projects with positive people.
+        </p>
+      </div>
       <ScrollTo
         destination={"projects-section"}
         title={"Projects - Adrian Tut"}
