a058a854186ae7a1de37c10e0876895499b52008
Adrian <adriantutuianu01@gmail.com>
2022-03-25 23:42:21 +0000

    Added grid in projects section

--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -7,7 +7,14 @@ const Projects = ({ setTitle }) => {
   return (
     <Element name="projects-section" id="projects" className="section">
       {" "}
-      Projects
+      <div className="projects-grid">
+        <div class="item item1">Item 1</div>
+        <div class="item item2">Item 2</div>
+        <div class="item item3">Item 3</div>
+        <div class="item item4">Item 4</div>
+        <div class="item item5">Item 5</div>
+        <div class="item item6">Item 6</div>
+      </div>
       <ScrollTo
         destination={"contact-section"}
         title={"Contact - Adrian Tut"}
