cb4fa9119dbf96fc69a585f6df7eb09fbbee7c5c
Adrian <adriantutuianu01@gmail.com>
2022-03-27 23:19:59 +0100

    Added animation from Lottie - home section

--- src/Sections/Home.js
+++ src/Sections/Home.js
@@ -2,8 +2,18 @@ import React from "react";
 import "./home.scss";
 import { Element } from "react-scroll";
 import ScrollTo from "../components/ScrollTo";
+import Lottie from "react-lottie";
+import * as animationData from "../icons/39610-design.json";
 
 const Home = ({ setTitle }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <Element name="home-section" id="home" className="section">
       {" "}
@@ -14,6 +24,7 @@ const Home = ({ setTitle }) => {
         </h2>
       </div>
       <div className="right-side">Right Side</div>
+      <Lottie options={defaultOptions} />
       <ScrollTo
         destination={"about-section"}
         title={"About - Adrian Tut"}
