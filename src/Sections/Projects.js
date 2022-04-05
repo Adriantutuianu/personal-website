47bb32027fc417da8d4adb29efb88244028e143b
Adrian <adriantutuianu01@gmail.com>
2022-04-05 23:57:37 +0100

    Dynamic background images

--- src/Sections/Projects.js
+++ src/Sections/Projects.js
@@ -7,26 +7,35 @@ const Projects = ({ setTitle }) => {
   const projects = [
     {
       name: "Product Management App",
+      imageName: "product-management",
       url: "https://product-management-app.adriantut.dev",
     },
     {
       name: "Shopping List",
+      imageName: "shopping-list",
       url: "https://shopping-list.adriantut.dev",
     },
     {
       name: "Random Quotes",
+      imageName: "random-quotes",
       url: "https://random-quotes.adriantut.dev",
     },
     {
       name: "Forkify App",
+      imageName: "forkify-app",
+
       url: "https://forkify-app.adriantut.dev",
     },
     {
       name: "Html & Css Homepage",
+      imageName: "homepage",
+
       url: "https://html-css-app.adriantut.dev",
     },
     {
       name: "Calculator App",
+      imageName: "calculator",
+
       url: "https://calculator-app.adriantut.dev",
     },
   ];
@@ -36,7 +45,7 @@ const Projects = ({ setTitle }) => {
       className={`item item${index}`}
       key={index}
       style={{
-        backgroundImage: "url(./project-images/test.jpg)",
+        backgroundImage: `url(./project-images/${project.imageName})`,
         backgroundSize: "cover",
       }}
     >
