5de7e462024f57cc31dc3cb8d5bfb3205b4dd2f8
Adrian <adriantutuianu01@gmail.com>
2022-04-21 00:18:50 +0100

    Attempt to fix production build

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import "./App.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import About from "./sections/About";
-import Home from "./sections/Home";
-import Projects from "./sections/Projects";
-import Contact from "./sections/Contact";
+import About from "./Sections/About";
+import Home from "./Sections/Home";
+import Projects from "./Sections/Projects";
+import Contact from "./Sections/Contact";
 
 function App() {
   const [title, setTitle] = useState("Adrian Tut - Web & Software Development");
